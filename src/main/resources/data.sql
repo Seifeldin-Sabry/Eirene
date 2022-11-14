86e0a855c80ea5e0f2e8b993a30d6dc50efb2b5d
Seifeldin Sabry <seifeldin.sabry@student.kdg.be>
2022-11-14 14:29:52 +0100

    encrypted the passwords in data.s

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO USERS (email, name, password, sex)
-VALUES ('se@gmail.com', 'Seif', 'password', 'MALE');
+VALUES ('se@gmail.com', 'Seif', '$2a$10$w5HLlv2qAv59t1WCPE.5aOwbGvGItDPnnNTZR5EmsT6AYHgicnT1O', 'MALE');
 
 INSERT INTO USERS (email, name, password, sex)
-VALUES ('p@gmail.com', 'Peter', 'password', 'MALE');
+VALUES ('p@gmail.com', 'Peter', '$2a$10$w5HLlv2qAv59t1WCPE.5aOwbGvGItDPnnNTZR5EmsT6AYHgicnT1O', 'MALE');
 
 INSERT INTO USERS (email, name, password, sex)
-VALUES ('f@gmail.com', 'Filip', 'password', 'MALE');
+VALUES ('f@gmail.com', 'Filip', '$2a$10$w5HLlv2qAv59t1WCPE.5aOwbGvGItDPnnNTZR5EmsT6AYHgicnT1O', 'MALE');
 
 INSERT INTO USERS (email, name, password, sex)
-VALUES ('e@gmail.com', 'Elina', 'password', 'FEMALE');
+VALUES ('e@gmail.com', 'Elina', '$2a$10$w5HLlv2qAv59t1WCPE.5aOwbGvGItDPnnNTZR5EmsT6AYHgicnT1O', 'FEMALE');
