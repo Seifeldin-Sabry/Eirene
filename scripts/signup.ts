6eb60cad48b618cd09251f21bc2ad3519c765de9
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2022-11-21 19:30:59 +0100

    fix: show both password fields in sign up

--- scripts/signup.ts
+++ scripts/signup.ts
@@ -1,4 +1,5 @@
 const password = document.querySelector('#password') as HTMLInputElement;
+const confirmPassword = document.querySelector('#confirmPassword') as HTMLInputElement;
 
 const eyeBtn = document.querySelector('#button-show-password') as HTMLButtonElement;
 const eyeIcon = eyeBtn.querySelector('.bi-eye') as HTMLElement;
@@ -7,11 +8,15 @@ let hidden = true;
 eyeBtn.addEventListener('click', () => {
     if (hidden) {
         password.setAttribute('type', 'text');
+        if (confirmPassword != null)
+            confirmPassword.setAttribute('type', 'text');
         eyeIcon.classList.remove('bi-eye');
         eyeIcon.classList.add('bi-eye-slash');
         hidden = false;
     } else {
         password.setAttribute('type', 'password');
+        if (confirmPassword != null)
+            confirmPassword.setAttribute('type', 'password');
         eyeIcon.classList.remove('bi-eye-slash');
         eyeIcon.classList.add('bi-eye');
         hidden = true;
