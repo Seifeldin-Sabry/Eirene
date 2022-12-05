de327c1e90750a01168e212ea95bd32b4bbe41b5
Elina Van der Taelen <elina.vandertaelen@student.kdg.be>
2022-12-05 15:04:52 +0000

    feat: rename session

---
+++ src/main/java/be/kdg/eirene/presenter/viewmodel/SessionEditViewModel.java
@@ -0,0 +1,12 @@
+package be.kdg.eirene.presenter.viewmodel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SessionEditViewModel {
+
+	private String sessionName;
+
+}
