621feaff3579d2ba5eb9c467446fb07cf7725e95
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2022-12-19 11:51:24 +0100

    feat: mocked datasender on dev profile with 'realistic' fake data

--- src/main/java/be/kdg/eirene/controllers/ActiveSessionMockController.java
+++ src/main/java/be/kdg/eirene/controllers/ActiveSessionMockController.java
@@ -8,6 +8,8 @@ import be.kdg.eirene.service.UserService;
 import be.kdg.eirene.service.evaluator.ReportGeneratorService;
 import be.kdg.eirene.util.ReadingValidator;
 import be.kdg.eirene.util.RequestDecryptor;
+import com.github.javafaker.Faker;
+import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
@@ -20,11 +22,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.sql.Timestamp;
 
-@Profile ("prod")
+@Profile ("dev")
 @RestController
 @RequestMapping ("/newsession")
-public class ActiveSessionController {
+public class ActiveSessionMockController {
 
 	private final ReportGeneratorService reportGenerator;
 	private final ReadingValidator validator;
@@ -33,10 +36,11 @@ public class ActiveSessionController {
 	private final UserService userService;
 	private final Logger logger;
 	private final RequestDecryptor decryptor;
+	private final Faker faker = new Faker();
 	private Session session;
 
 	@Autowired
-	public ActiveSessionController(RequestDecryptor decryptor, CookieService cookieService, UserService userService, SessionService sessionService, ReportGeneratorService reportGenerator, ReadingValidator validator) {
+	public ActiveSessionMockController(RequestDecryptor decryptor, CookieService cookieService, UserService userService, SessionService sessionService, ReportGeneratorService reportGenerator, ReadingValidator validator) {
 		this.decryptor = decryptor;
 		this.cookieService = cookieService;
 		this.userService = userService;
@@ -64,8 +68,28 @@ public class ActiveSessionController {
 
 	@GetMapping ("/api")
 	public String getChartData() {
+		Reading currentReading = new Reading(
+				new Timestamp(System.currentTimeMillis()),
+				new BrainWaveReading(
+						faker.number().numberBetween(0, 200),
+						faker.number().numberBetween(0, 100),
+						faker.number().numberBetween(0, 100)),
+				new SensorData(
+						faker.number().numberBetween(40, 180),
+						faker.number().numberBetween(18, 25),
+						faker.number().numberBetween(52, 53),
+						faker.number().numberBetween(30, 60),
+						faker.number().numberBetween(0, 1)));
+
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").setPrettyPrinting().create();
+
+		final String readingJSON = gson.toJson(currentReading);
+		HttpRequest request = HttpRequest.post("http://localhost:8081/newsession")
+		                                 .contentType("application/json")
+		                                 .send(readingJSON);
+		logger.info(String.valueOf(request.code()));
+
 		EvaluatedData data = reportGenerator.formulateReport(session.getReadings(), session.getType());
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		return gson.toJson(data);
 	}
 
@@ -73,6 +97,7 @@ public class ActiveSessionController {
 	public void getData(@RequestBody Reading data) {
 		if (session != null && validator.validate(data)) {
 			session.addReading(data);
+			logger.info("Added reading: " + data);
 		}
 	}
 
